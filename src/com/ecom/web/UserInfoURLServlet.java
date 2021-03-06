25bd295688fc95898e22b3f7adb7630165dd369e
Tejaswee Shelar <tejaswee233@gmail.com>
2021-03-06 18:22:35 +0530

    url rewrite

---
+++ src/com/ecom/web/UserInfoURLServlet.java
@@ -0,0 +1,54 @@
+package com.ecom.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserInfoURLServlet
+ */
+@WebServlet("/user-info")
+public class UserInfoURLServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserInfoURLServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		try {
+			response.setContentType("text/html");
+			PrintWriter out=response.getWriter();
+			
+			String userId= request.getParameter("userId");
+			String userName= request.getParameter("name");
+			
+			out.println("<a href='profile?userId="+userId+"&userName="+userName+"'>Go to Profile</a>");
+			out.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
